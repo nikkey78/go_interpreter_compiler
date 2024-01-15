bd64469a7cffe5153e1abd3f238e0886fe5af2dd
nikkey78 <nick-bazh@yandex.ru>
2024-01-15 15:27:26 +0300

    added repl

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"console"
+	"fmt"
+	"monkey2/repl"
+	"os"
+	"os/user"
+)
+
 func main() {
-	
+
+	defer console.GreenReset()()
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Println("Feel free type in commnands")
+	repl.Start(os.Stdin, os.Stdout)
 }
