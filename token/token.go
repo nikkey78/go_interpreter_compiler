b31de3811457cb2df3ff1ca9d6e91858effe9215
nikkey78 <nick-bazh@yandex.ru>
2024-01-25 15:01:16 +0300

    Supporting Strings in our Lexer

--- token/token.go
+++ token/token.go
@@ -12,8 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// Identifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y...
-	INT   = "INT"   // 123453
+	IDENT  = "IDENT" // add, foobar, x, y...
+	INT    = "INT"   // 123453
+	STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="
