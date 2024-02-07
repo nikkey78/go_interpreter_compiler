8904e1102240576a8bc0298de78cfeaf070330a8
nikkey78 <nick-bazh@yandex.ru>
2024-02-07 15:40:50 +0300

    Arrays

--- code/code.go
+++ code/code.go
@@ -27,6 +27,7 @@ const (
 	OpNull
 	OpGetGlobal
 	OpSetGlobal
+	OpArray
 )
 
 type Definition struct {
@@ -59,6 +60,8 @@ var definitions = map[Opcode]*Definition{
 
 	OpGetGlobal: {"OpGetGlobal", []int{2}},
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
+
+	OpArray: {"OpArray", []int{2}},
 }
 
 type Instructions []byte
