161767987b042602aed070ed52b76d19cd47658d
nikkey78 <nick-bazh@yandex.ru>
2024-02-12 12:21:51 +0300

    Function without Return

--- vm/vm.go
+++ vm/vm.go
@@ -222,6 +222,15 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
+		case code.OpReturn:
+			vm.popFrame()
+			vm.pop()
+
+			err := vm.push(Null)
+			if err != nil {
+				return err
+			}
 		}
 	}
 	return nil
