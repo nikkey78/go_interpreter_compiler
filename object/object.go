7dd905a6a9ad6b27833b9c778f404c9f31f6ff4b
nikkey78 <nick-bazh@yandex.ru>
2024-02-13 13:35:03 +0300

    Arguments in the VM

--- object/object.go
+++ object/object.go
@@ -90,6 +90,7 @@ func (f *Function) Inspect() string {
 type CompiledFunction struct {
 	Instructions code.Instructions
 	NumLocals int
+	NumParameters int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
