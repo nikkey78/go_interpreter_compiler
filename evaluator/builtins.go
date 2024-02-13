da1f3dc80d702b20a87fc58bc495a6569fa6889d
nikkey78 <nick-bazh@yandex.ru>
2024-02-13 15:36:43 +0300

    remove builtin functions to object package

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -109,5 +109,10 @@ import (
 // }
 
 var builtins = map[string]*object.Builtin{
-	"len": object.GetBuiltinByName("len"),
+	"len":   object.GetBuiltinByName("len"),
+	"puts":  object.GetBuiltinByName("puts"),
+	"first": object.GetBuiltinByName("first"),
+	"last":  object.GetBuiltinByName("last"),
+	"rest":  object.GetBuiltinByName("rest"),
+	"push":  object.GetBuiltinByName("push"),
 }
